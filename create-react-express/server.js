cc927bc41cc6e46fbe4ef4e8a10683c02f23730b
Josh Loftis <jshlfts32@gmail.com>
2018-01-14 15:09:54 -0500

    Beginning of the graphql server

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
-const PORT = process.env.PORT || 3001;
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books },
+};
+
+const PORT = process.env.PORT || 4000;
 const app = express();
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
