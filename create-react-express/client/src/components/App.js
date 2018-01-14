18fb4f6859245bdd4d0aa59258a05cd34f6fdde3
Acard1990 <scott.cardinali@gmail.com>
2018-01-14 10:28:20 -0500

    adding boilderplate to app.js

--- create-react-express/client/src/components/App.js
+++ create-react-express/client/src/components/App.js
@@ -1,1 +1,26 @@
-git 
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './cardheader';
+import Body from './cardbody';
+import Agreement from './agreement';
+import Button from './cardbutton';
+import Esign from './esign';
+import Input from './input';
+import Payments from './monthlypayment';
+import Slider from './slider';
+import Table from './table';
+import Terms from './terms';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default App;
