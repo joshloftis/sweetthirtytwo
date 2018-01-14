c3f055e92e2bc60f8841a7629ee8dcfd818e99d2
Acard1990 <scott.cardinali@gmail.com>
2018-01-14 12:40:01 -0500

    working on header

--- create-react-express/client/src/components/App.js
+++ create-react-express/client/src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Header from './cardheader';
 import Body from './cardbody';
 import Agreement from './agreement';
--- create-react-express/client/src/components/cardheader.js
+++ create-react-express/client/src/components/cardheader.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import App from './app';
+import App from './App';
+import '../css/cardheader.css';
 
 const Header = props =>
-    <div className="App">
-<       div className="App-header">
-            <img src="" className="" alt="logo" />
-        <h2>Suite32</h2>
-    </div>
+    <container className="App">
+        <div className="App-header">
+            {/* Both of the img's below should be the logo icons in the mockup */}
+            <img src="" className="App-logo" alt="logo" />
+            <img src="" className="App-logo" alt="logo" />
+            <h2 className="title">Suite32</h2>
+            <h4 className="addcustomer"> + Add New Customer</h4>
+        </div>
         <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-    </div>
+    </container>
 
 export default Header;
