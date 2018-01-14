041358aa6c2c8bcb7179d8fc663e92ca50cc4610
Acard1990 <scott.cardinali@gmail.com>
2018-01-14 10:42:58 -0500

    fixing small errors

--- create-react-express/client/src/components/cardheader.js
+++ create-react-express/client/src/components/cardheader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import App from './app';
 
-Const Header = props =>
+const Header = props =>
     <div className="App">
 <       div className="App-header">
-            <img src= className="" alt="logo" />
+            <img src="" className="" alt="logo" />
         <h2>Suite32</h2>
     </div>
         <p className="App-intro">
