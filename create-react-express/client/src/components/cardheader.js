cd48b6582e3a429ae127bf8c5653d4ce55d98177
Acard1990 <scott.cardinali@gmail.com>
2018-01-15 13:25:05 -0500

    removed pointless stuff

--- create-react-express/client/src/components/cardheader.js
+++ create-react-express/client/src/components/cardheader.js
@@ -3,7 +3,7 @@ import App from './App';
 import '../css/cardheader.css';
 
 const Header = props =>
-    <container className="App">
+    <div className="App">
         <div className="App-header">
             {/* Both of the img's below should be the logo icons in the mockup */}
             <img src="" className="App-logo" alt="logo" />
@@ -11,9 +11,6 @@ const Header = props =>
             <h2 className="title">Suite32</h2>
             <h4 className="addcustomer"> + Add New Customer</h4>
         </div>
-        <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-    </container>
+    </div>
 
 export default Header;
