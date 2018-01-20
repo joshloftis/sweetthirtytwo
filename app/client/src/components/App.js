04816498c0d35b339ec0a758abd646b9ca1921dc
Acard1990 <scott.cardinali@gmail.com>
2018-01-20 12:34:09 -0500

    commented out slider to compile

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -7,7 +7,7 @@ import Button from './cardbutton';
 import Esign from './esign';
 import Input from './input';
 import Payments from './monthlypayment';
-import Slider from './slider';
+//import Slider from './slider';
 import Table from './table';
 import Terms from './terms';
 import Cardlayout from './cardlayout';
