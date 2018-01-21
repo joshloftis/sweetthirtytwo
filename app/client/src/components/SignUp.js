533aa6d306caf3a95f105bae7b2d99d3125b95d8
Acard1990 <scott.cardinali@gmail.com>
2018-01-21 09:20:25 -0500

    styled signup page

--- app/client/src/components/SignUp.js
+++ app/client/src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import '../css/login.css';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -45,41 +46,68 @@ class SignUp extends React.Component {
 
     render() {
       return (
+        <main className="mainspace black-80">
+        <form class="measure center" />
+          <fieldset id="sign_up" class="ba b--transparent ph0 mh0" />
+          <legend className="f4 fw6 ph0 mh0">Sign Up</legend>
         <div>
           <form>
+          <div className="mt3">
+          <label classNAme="db fw6 lh-copy f6" for="firstname">First Name</label>
             <input type="text"
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               placeholder="First Name"
               name="firstName"
               value={this.state.firstName}
               onChange={this.handleInputChange}
               required/>
+          </div>
+          <div className="mt3">
+          <label classNAme="db fw6 lh-copy f6" for="lastname">Last Name</label>
             <input type="text"
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               placeholder="Last Name"
               name="lastName"
               value={this.state.lastName}
               onChange={this.handleInputChange}
               required/>
+          </div>
+          <div className="mt3">
+          <label classNAme="db fw6 lh-copy f6" for="email">Email</label>
             <input type="text"
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               placeholder="Email Address"
               name="email"
               value={this.state.email}
               onChange={this.handleInputChange}
               required/>
+          </div>
+          <div className="mt3">
+          <label classNAme="db fw6 lh-copy f6" for="username">Username</label>
             <input type="text"
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               placeholder="Username"
               name="username"
               value={this.state.username}
               onChange={this.handleInputChange}
               required/>
+          </div>
+          <div className="mt3">
+          <label classNAme="db fw6 lh-copy f6" for="password">Password</label>
             <input type="password"
+              className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               placeholder="Password"
               name="password"
               value={this.state.password}
               onChange={this.handleInputChange}
               required/>
-            <button type="Submit" onClick={this.onClick}>Submit</button>
+          </div>
           </form>
         </div>
+        <div className="">
+          <input onClick={this.onClick} className="spacer b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+        </div>
+      </main>
       );
     }
   }
