ead4d2d3ab4523dd246e686a85b4760e333a0b31
Acard1990 <scott.cardinali@gmail.com>
2018-01-21 18:15:53 -0500

    updated colorsplit and hover

--- app/client/src/components/cardbody.js
+++ app/client/src/components/cardbody.js
@@ -5,8 +5,8 @@ import '../css/cardbody.css';
 
 const Cardbody = props => 
     <div>
-        <span class="viz">
-        <article className="measure mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <span className="viz">
+        <article className="colorsplit measure mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
             <div className="tc">
                 <h4 className="f4">Show Date for next appointment.</h4>    
                 <img src="https://vignette.wikia.nocookie.net/powerlisting/images/7/7a/Daenerys_Targaryen_Game_of_Thrones_Fire_Immunity.jpg/revision/latest?cb=20160629183606" 
