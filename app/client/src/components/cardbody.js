3912df749bc33635b84a3ef0f13f9567f7e34460
Acard1990 <scott.cardinali@gmail.com>
2018-01-25 21:23:56 -0500

    fixed more styling before making dynamic

--- app/client/src/components/cardbody.js
+++ app/client/src/components/cardbody.js
@@ -8,11 +8,11 @@ const CardBody = props =>
         <span className="viz">
         <article className="colorsplit measure mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
             <div className="tc">
-                <h4 className="f4">Show Date for next appointment.</h4>    
+                <h4>Show Date for next appointment.</h4>    
                 <img src="https://vignette.wikia.nocookie.net/powerlisting/images/7/7a/Daenerys_Targaryen_Game_of_Thrones_Fire_Immunity.jpg/revision/latest?cb=20160629183606" 
                 className="hideme showme br-100 h3 w3 dib" 
                 title="Photo of daenerys targaryen"/>
-                <h4 className="f4">Customer Name</h4>
+                <h4>Customer Name</h4>
                 <h5 className="hideme showme">Show Total Cost/Bill.</h5>
                 <hr className="mw4 bb bw1 b--black-10"/>
             </div>
