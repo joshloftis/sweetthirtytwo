75a2be8da05ab22845094b523a1fcb1b670e802d
Acard1990 <scott.cardinali@gmail.com>
2018-01-26 18:25:10 -0500

    added animation to show sections, fixed buttons

--- app/client/src/components/cardbutton.js
+++ app/client/src/components/cardbutton.js
@@ -4,10 +4,10 @@ import '../css/cardbutton.css';
 
 const CardButton = props => 
 <span className="viz">
-    <div className="ph3 mb4">
-        <a className="f6 link dim ba pv2 mb2 navy ttu" href="#0">Review</a>
-        <a className="f6 link dim ba pv2 mb2 navy ttu" href="#0">Send</a>
-    </div>
+    <div className="alpha">
+        <a className="btn f6 dim link ba pv2 mb2 navy ttu" href="#0">Review</a>
+        <a className="btn f6 dim link ba pv2 mb2 navy ttu" href="#0">Send</a>
+   </div>
 </span>;
 
 export default CardButton;
