4ed809235d9af269b78ac2a5e5ffc40d6468099e
Josh Loftis <jshlfts32@gmail.com>
2018-01-26 20:39:59 -0500

    Updated package json

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,25 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './components/App';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
 import './css/index.css';
 
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
 });
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
+const authLink = setContext((root, { headers }) => {
   const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
