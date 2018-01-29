9cbd2111373b968c9c55fb092d9c4e5eaa09a4c6
Acard1990 <scott.cardinali@gmail.com>
2018-01-28 20:29:51 -0500

    fixed styling and hover effects. now, only hover in active visual space will set off dropdown

--- app/client/src/components/cardbody.js
+++ app/client/src/components/cardbody.js
@@ -12,27 +12,25 @@ const CardBody = props =>
   // }
   (
     <div>
-      <span className="card colorsplit">
-        <article className="colorsplit mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <article className="viz animate colorsplit mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
           <div className="tc">
             <img
               src="https://vignette.wikia.nocookie.net/powerlisting/images/7/7a/Daenerys_Targaryen_Game_of_Thrones_Fire_Immunity.jpg/revision/latest?cb=20160629183606"
-              className="hideme showme br-100 h3 w3 dib"
+              className="br-100 h3 w3 dib"
               title="Photo of daenerys targaryen"
               alt="Profile"
             />
-            <h4 className="a">Mikal Bright</h4>
-            <h5 className="b">Total: 5,567</h5>
-            <h5 className="c">Fees: 300</h5>
-            <h5 className="d">Insurance: 500</h5>
-            <h5 className="e">Down Payment: 500</h5>
-            <h5 className="f">Range: 12</h5>
-            <h5 className="g">Monthly Payment: 48.98</h5>
-            <hr className="h mw4 bb bw1 b--black-10" />
+            <h4>Mikal Bright</h4>
+            <hr className="mw4 bb bw1 b--black-10" />
+            <h5 className="alpha">Total: 5,567</h5>
+            <h5 className="beta">Fees: 300</h5>
+            <h5 className="charlie">Insurance: 500</h5>
+            <h5 className="delta">Down Payment: 500</h5>
+            <h5 className="echo">Range: 12</h5>
+            <h5 className="foxtrot">Monthly Payment: 48.98</h5>
           </div>
           <CardButton />
         </article>
-      </span>
     </div>
   );
 export default CardBody;
