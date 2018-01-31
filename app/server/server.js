1ffb365856162d5003f4612bafdad2515a59ecaa
Josh Loftis <jshlfts32@gmail.com>
2018-01-31 16:56:47 -0500

    Working demo of REST login/signup. Needs work

--- app/server/server.js
+++ app/server/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const path = require('path');
-const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const jwtExp = require('express-jwt');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+const exphbs = require('express-handlebars');
+
+const morgan = require('morgan');
 const cors = require('cors');
-const jwt = require('express-jwt');
+const path = require('path');
 const dotenv = require('dotenv').config();
 
-const schema = require('./graphql/schema');
+// Requiring our models for syncing
 const db = require('./models');
+const schema = require('./graphql/schema');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -17,10 +23,43 @@ app.use(morgan('dev'));
 
 app.use('*', cors({ origin: 'http://localhost:3000' }));
 
+app.set('views', 'app/server/views');
+app.engine('handlebars', exphbs({}));
+app.set('view engine', 'handlebars');
+
+// I don't care about your HTTP Method
+app.use(cookieParser(process.env.JWT_SECRET));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use('/auth', require('./routes/auth-routes.js'));
+
+app.get('/', jwtExp({
+  secret: process.env.JWT_SECRET,
+  getToken: function fromCookie(req) {
+    if (req.signedCookies) {
+      return req.signedCookies.jwtAuthToken;
+    }
+    return null;
+  },
+  credentialsRequired: false,
+}), (req, res, next) => {
+  // if user is signed-in, next()
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/auth/sign-in');
+  }
+});
+
 app.use(
   '/graphql',
   bodyParser.json(),
-  jwt({
+  jwtExp({
     secret: process.env.JWT_SECRET,
     credentialsRequired: false,
   }),
@@ -31,7 +70,7 @@ app.use(
         db.User.findOne({ _id: req.user.id }) : Promise.resolve(null),
     },
   })),
-  jwt({
+  jwtExp({
     secret: process.env.JWT_SECRET,
     credentialsRequired: false,
   }),
